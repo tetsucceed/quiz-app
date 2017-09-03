1ebddac8ae75160058294efb0d4f5e68bb3ca4f7
tetsucceed <tetsucceed@gmail.com>
2017-09-04 01:31:44 +0300

    Add models

--- qapp/quiz_view/admin.py
+++ qapp/quiz_view/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import * 
+
+admin.site.register(Quiz)
+admin.site.register(Questions)
+admin.site.register(Answers)
+
 # Register your models here.
--- qapp/quiz_view/models.py
+++ qapp/quiz_view/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Quiz(models.Model):
+	title = models.CharField(max_length=255, unique=True)
+	questions = models.ForeignKey('Questions', on_delete=models.CASCADE)
+
+class Questions(models.Model):
+	question_text = models.CharField(max_length=255, unique=True)
+	answers =  models.ForeignKey('Answers', on_delete=models.CASCADE)
+
+class Answers(models.Model):
+	answer_text = models.CharField(max_length=255, unique=True)
+	weight = models.IntegerField(default=0)
+	choice_int = models.IntegerField(default=0)
+
 # Create your models here.
